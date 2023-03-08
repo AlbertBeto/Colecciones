62eff8f86cbbca63d4cab45519671c71ad8dc782
Beto <beto@beto.cat>
2023-03-09 00:35:11 +0100

    Vamos a por el menu en el main de Maps

--- src/Mapas/Main.java
+++ src/Mapas/Main.java
@@ -8,11 +8,13 @@ public class Main {
 
 
     public Main() {
+        Ubicacion exit = new Ubicacion(0,"Estás sentado en la clase de programación");
         Ubicacion montaña = new Ubicacion(1,"Estás en la cima de la montaña");
         Ubicacion playa = new Ubicacion(2,"Estás bañandote en la playa");
         Ubicacion edificio = new Ubicacion(3,"Estás dentro de un edificio muy alto");
         Ubicacion puente = new Ubicacion(4,"Estas de pie en un puente");
         Ubicacion bosque = new Ubicacion(5,"Estás en un bosque");
+        ubicaciones.put(0,exit);
         ubicaciones.put(1,montaña);
         ubicaciones.put(2,playa);
         ubicaciones.put(3,edificio);
@@ -22,14 +24,18 @@ public class Main {
         montaña.addExit("O",2);
         montaña.addExit("S",4);
         montaña.addExit("E",3);
+        montaña.addExit("Q",0);
         playa.addExit("N",5);
         playa.addExit("S",4);
         playa.addExit("E",1);
+        playa.addExit("Q",0);
         edificio.addExit("O",1);
+        edificio.addExit("Q",0);
         puente.addExit("N",1);
         puente.addExit("O",2);
         bosque.addExit("O",2);
         bosque.addExit("S",1);
+        bosque.addExit("Q",0);
     }
 
 
